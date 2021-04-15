61a6e189121f71f142e63e027345693bcb55f392
hySung <up1ofpt@hanmail.net>
2021-04-15 23:54:21 +0900

    commit on main

--- reactPj/rpj1/src/components/App.js
+++ reactPj/rpj1/src/components/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 //var React=require('react'); //동일한 문법 
 //export default class App extends React.Component{
  class App extends React.Component{  
+    constructor(props){
+        super(props);
+        this.state ={
+            name:''
+        }
+    }
+
     render(){
             return (
                 <div>
-                <h1>Hello!!!</h1>
+                <button onClick={()=>{this.setState({name:'hwayeon'});}}>clcick here</button>
+                <h1>Hello!!f오예dggdfdfgg??사!중!!{this.state.name}</h1>
                 </div>
             );
     }
 }
+
+
 export default App;//이 클래스를 다른 데서도 사용할수 있게  export
 //module.export=App; //동일한 문법
--- reactPj/rpj1/webpack.config.js
+++ reactPj/rpj1/webpack.config.js
@@ -1,5 +1,6 @@
 var webpack=require('webpack'); //webpack 불러오기
-const path=require('path');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const path      =require('path');
 
 
 module.exports ={
@@ -11,29 +12,42 @@ module.exports ={
       publicPath: '/public/',
       filename:'bundle.js'              //output으로 내놓는 파일 추후 script src=""로 가져와서 쓰게된다.
     },
-    devtool: 'inline-source-map',
     devServer:{                         //게빌서버의 설정
         hot:true,                       //리로딩할때마다
         inline:true,
-        host: 'localhost',              //기본값은 로컬ip
+        host: '127.0.0.1',              //기본값은 로컬ip
         port: 8080,                     //개발서버의 포트
         open: true,                     // open page when start
         contentBase:__dirname+'/public/'//index파일의 위치
     },
     module:{
-        rules:[
-          {
-          test:/\.js|jsx$/,
-          exclude:/node_modules/,
-          use: ['babel-loader']
-          }
+        rules: [
+            {
+                test:/\.js|jsx$/,        
+                exclude:/node_modules/,
+                //use: ['babel-loader']
+                /*loader: 'babel-loader',
+                options: {
+                    plugins:['react-hot-loader/babel'],
+                    cacheDirectory:true
+                }*/
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: { plugins: [ 'react-refresh/babel' ] }
+                    }
+                ]
+              }            
         ]
     },
 
     plugins:[// 자동으로 리로딩하는 기능()
-        new webpack.DefinePlugin({ 
+       /* new webpack.DefinePlugin({ 
             'process.env.NODE_ENV': JSON.stringify('development')
         })
+        */
+         new webpack.HotModuleReplacementPlugin() //새로고침시 변경된사항 적용
+        ,new ReactRefreshWebpackPlugin()
     ]
 
 };
