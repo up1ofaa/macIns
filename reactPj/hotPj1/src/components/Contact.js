f8fe9471151125e75392159c02d93ecc61f23d33
hySung <up1ofpt@hanmail.net>
2021-04-19 21:41:58 +0900

    commit on main

--- reactPj/hotPj1/src/components/Contact.js
+++ reactPj/hotPj1/src/components/Contact.js
@@ -38,7 +38,7 @@ export default class Contact extends React.Component{
             data=data.filter(
                 (contact) => {
                     return contact.name.toLowerCase()
-                    .indexOf(this.state.keyword)>-1;
+                    .indexOf(this.state.keyword.toLowerCase())>-1;
                     }
                 );
             return data.map((contact,i)=>{
@@ -50,7 +50,7 @@ export default class Contact extends React.Component{
         return (
             <div>
                 <h1>Contacts</h1>
-                <input  name="keyword" 
+                <input  name="ipt_search" 
                         placeholder="search"
                         value={this.state.keyword}
                         onChange ={this.handleChnage}
@@ -61,3 +61,30 @@ export default class Contact extends React.Component{
     }
 
 }
+
+/*arr.sort(compareFunction(a,b))
+
+function return값이 0보다 낮은경우, a를 b보다 낮은 색인정렬, a가 우선
+0을 반환하면 위치변경없음
+0보다 큰경우  b를 ㅂ다 낮은 인덱스로 소트
+
+*/
+
+/* arr.filter(callback(element[,index[,array]])[,thisArg])
+callback :각 요소를 시험할 함수. true를 반환하면 요소를 유지하고, false를 반환하면 버립니다. 
+element :처리할 현재요소
+index : 처리할 현재요소의 인덱스
+array : filter를 호출한 배열
+thisArg : callback 을 실행할 때 this로 사용하는값
+
+*/
+
+/*
+검색조건에 따른 배열 필터링
+function filterItems(query){
+    return fruits.filter(function(el){
+        return el.toLowerCase().indexOf(query.toLowerCase()) >-1;
+    })
+}
+
+**/
--- reactPj/hotPj1/src/components/ContactInfo.js
+++ reactPj/hotPj1/src/components/ContactInfo.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class ContactInfo extends React.Component{
     render(){
         return(
-            <div>{this.props.contact.name}  {this.props.contact.phone}</div>
+            <div><h4>{this.props.contact.name}  {this.props.contact.phone}</h4></div>
         );
     } 
 }
